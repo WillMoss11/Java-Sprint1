7b107ee107616a6582f9b871c011e8ba5e583eb0
William Moss <you@example.com>
2024-10-29 21:17:26 -0230

    Docs...Lots of Docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"library","l":"Author"},{"p":"library","l":"Book"},{"p":"library","l":"Demo"},{"p":"library","l":"Employee"},{"p":"library","l":"Library"},{"p":"library","l":"LibraryItem"},{"p":"library","l":"Patron"},{"p":"library","l":"Periodical"},{"p":"library","l":"Student"}];updateSearchResults();
