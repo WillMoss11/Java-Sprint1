425ec58eb87a35aa81c7033dbfe63dbbcda2cec6
William Moss <you@example.com>
2024-10-31 23:05:46 -0230

    #

--- src/library/PeriodicalMaster.java
+++
@@ -1,8 +0,0 @@
-package library;
-
-public abstract class PeriodicalMaster extends LibraryItem {
-    public PeriodicalMaster(String id, String title, String author, String isbn, String publisher, int numCopies) {
-        super(id, title, author, isbn, publisher, numCopies);
-    }
-
-}
