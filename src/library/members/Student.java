f7afb9629727702e9163dccc89f62d31d650d7b3
William Moss <you@example.com>
2024-10-31 23:04:44 -0230

    Changed

---
+++ src/library/members/Student.java
@@ -0,0 +1,14 @@
+package library.members;
+
+public class Student extends Member {
+    public Student(String name, String address, String phone, String memberId) {
+        super(name, address, phone, memberId);
+    }
+}
+
+
+
+
+
+
+
