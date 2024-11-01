06567c01f0b61525ca263d72bfceeacc537327e3
William Moss <you@example.com>
2024-10-31 23:03:44 -0230

    Fixes

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,70 +1,75 @@
 package library;
 
+import library.items.LibraryItem;
+import library.members.Member;
+import library.models.Author;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Library {
     private List<LibraryItem> items;
+    private List<Member> members;
     private List<Author> authors;
-    private HashMap<Patron, List<LibraryItem>> borrowedItems;
 
     public Library() {
         items = new ArrayList<>();
+        members = new ArrayList<>();
         authors = new ArrayList<>();
-        borrowedItems = new HashMap<>();
     }
 
+    // Add a library item
     public void addItem(LibraryItem item) {
         items.add(item);
     }
 
+    // Add a member
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    // Add an author
     public void addAuthor(Author author) {
         authors.add(author);
     }
 
-    public boolean borrowItem(String title, Patron patron) {
-        for (LibraryItem item : items) {
-            if (item.getTitle().equalsIgnoreCase(title) && item.getNumCopies() > 0) {
-                item.setNumCopies(item.getNumCopies() - 1);
-                borrowedItems.putIfAbsent(patron, new ArrayList<>());
-                borrowedItems.get(patron).add(item);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void returnItem(String title, Patron patron) {
-        if (borrowedItems.containsKey(patron)) {
-            List<LibraryItem> borrowed = borrowedItems.get(patron);
-            for (LibraryItem item : borrowed) {
-                if (item.getTitle().equalsIgnoreCase(title)) {
-                    borrowed.remove(item);
-                    item.setNumCopies(item.getNumCopies() + 1);
-                    break;
-                }
-            }
-        }
-    }
-
-    public boolean deleteItemByTitle(String title) {
-        for (LibraryItem item : items) {
-            if (item.getTitle().equalsIgnoreCase(title)) {
-                items.remove(item);
-                return true; // Item found and removed
-            }
-        }
-        return false; // Item not found
-    }
-
+    // Get all library items
     public List<LibraryItem> getItems() {
         return items;
     }
 
+    // Get all members
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    // Get all authors
     public List<Author> getAuthors() {
         return authors;
     }
+
+    // Borrow an item
+    public void borrowItem(Member member, LibraryItem item, int quantity) {
+        // Check if the item is available
+        if (item.getNumCopies() >= quantity) {
+            member.borrowItem(item, quantity);
+            item.setNumCopies(item.getNumCopies() - quantity);
+            System.out.println(member.getName() + " borrowed " + quantity + " copies of " + item.getTitle());
+        } else {
+            System.out.println("Not enough copies available for " + item.getTitle());
+        }
+    }
+
+    // Return an item
+    public void returnItem(Member member, LibraryItem item, int quantity) {
+        member.returnItem(item, quantity);
+        item.setNumCopies(item.getNumCopies() + quantity);
+        System.out.println(member.getName() + " returned " + quantity + " copies of " + item.getTitle());
+    }
 }
+
+
+
+
 
 
